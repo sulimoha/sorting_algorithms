43b2c7d52ae16e8a2e5f5a872fb2eba78adb9cd0
Mohamed <sulimmoh@gmail.com>
2022-11-08 11:27:15 -0600

    3-quick_sort.c

--- sort.h
+++ sort.h
@@ -25,5 +25,12 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+
+/* helper functions */
+void qs(int *arr, int low, int high, size_t size);
+int partition(int *arr, int low, int high, size_t size);
 
 #endif
